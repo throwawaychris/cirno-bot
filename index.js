efcae3f4c2a9fd0ddadcaf5a291f22cd2f0ae277
throwawaychris <chrischo49@gmail.com>
2023-05-22 11:10:28 -0700

    Added gay command along with components

--- index.js
+++ index.js
@@ -10,6 +10,7 @@ const path = require('node:path');
 // Require the necessary discord.js classes
 const { Client, Collection, Events, GatewayIntentBits } = require('discord.js');
 const { token } = require('./config.json');
+const Canvas = require('@napi-rs/canvas');
 
 // Create a new client instance
 const client = new Client({ intents: [GatewayIntentBits.Guilds] });
