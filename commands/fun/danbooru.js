f2907a92cc2e41fd955c4e12f4c8aa79768ba405
throwawaychris <chrischo49@gmail.com>
2023-06-10 01:12:09 -0700

    Completed danbooru command

--- commands/fun/danbooru.js
+++ commands/fun/danbooru.js
@@ -4,13 +4,78 @@ module.exports = {
     data: new SlashCommandBuilder()
         .setName('danbooru')
         .setDescription('Retrives a random danbooru image')
+        .addBooleanOption(option =>
+            option
+                .setName('nsfw')
+                .setDescription('Allow NSFW posts'))
         .addStringOption(option =>
             option
                 .setName('tag')
-                .setDescription('Define a tag for the image')),
+                .setDescription('Define a tag for the image'))
+        .setDefaultMemberPermissions(0),
     async execute(interaction) {
-        const url = 'https://safebooru.donmai.us/posts/random?tags=is:sfw+cirno';
+        let url = 'https://safebooru.donmai.us/posts/random.json?tags=';
+        let response;
+        let data;
+        let danbooruColor = 0x3176f5;
 
-        await interaction.reply(url);
+        // Construct url
+        if (interaction.options.getBoolean('nsfw')) {
+            url = 'https://danbooru.donmai.us/posts/random.json?tags=is:nsfw';
+            danbooruColor = 0xf5318c;
+        }
+
+        if (!(interaction.options.getString('tag') === null)) {
+            url += `+${interaction.options.getString('tag')}`;
+        }
+        else {
+            url += `+order:rank`;
+        }
+
+        //console.log(interaction.options.getBoolean('nsfw'));
+        //console.log(url);
+
+        //url = `https://danbooru.donmai.us/posts/2360472.json`;
+
+        // fetch(url) TRY
+        try {
+            response = await fetch(url);
+            data = await response.json();
+        }
+        catch (error) {
+            console.log(error);
+            await interaction.reply({content: `*Something went wrong!*`, ephemeral: true});
+        }
+
+        // End early with error reply if no fetch with url
+        if (data.success === false) {
+            await interaction.reply({content: `*Couldn't fetch; [Bad tags?](https://danbooru.donmai.us/wiki_pages/howto%3Atag)*`, ephemeral: true});
+        }
+
+        // Construct embed
+        const danbooruEmbed = {
+            color: danbooruColor,
+            url: `https://danbooru.donmai.us/posts/${data.id}`,
+            image: {
+                url: `${data.file_url}`
+            },
+            title: `Artist: ${data.tag_string_artist}`,
+            footer: {
+                text: `Created: ${data.created_at}`
+            },
+        }
+
+        //console.log(data.tag_string_ch)
+
+        // Post embed
+        //console.log(data);
+        try {
+            await interaction.reply({embeds: [danbooruEmbed]});
+        } catch (error) {
+            console.log(error);
+            await interaction.reply({content: `***Opps**, found a gold-account restricted picture!*`, ephemeral: true});
+        }
+
+        //await interaction.followUp({content: `${url}`, ephemeral: true});
     }
 }
