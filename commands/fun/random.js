f4fda21fe683b3bbb41a1997ae2d1b0ba1ba5cfb
throwawaychris <chrischo49@gmail.com>
2023-06-15 16:20:07 -0700

    added multiple random number generation to random

--- commands/fun/random.js
+++ commands/fun/random.js
@@ -8,28 +8,52 @@ module.exports = {
             option
                 .setName('max')
                 .setDescription('The max value of the random')
-                .setRequired(true)),
+                .setRequired(true))
+        .addIntegerOption(option =>
+            option
+                .setName('repeat')
+                .setDescription('The amount of random numbers to generate (default: 1)')),
     async execute(interaction) {
         //random number
-        const max = interaction.options.getInteger('max');
-        let d = Math.floor((Math.random()*max)+1);
+        const MAX = interaction.options.getInteger('max');
+        const REPEAT = interaction.options.getInteger('repeat')>0 ? interaction.options.getInteger('repeat') : 1;
+        //console.log(`repeat: ${REPEAT}`);
+        
+        let d, d_msg = "";
         msg = "";
         last = -1;
-        repeat = 0;
+        dubs = 0;
+        n = 0;
         
-        await interaction.reply(`**${d}**`);
+        for (let i = 0; i < REPEAT; i++) {
+            //console.log(`Entered for loop: ${i}`);
+            d = Math.floor((Math.random()*MAX)+1);
+            d_msg += `**${d}**\n`;
 
-        while (d > 0) {            
-            last = d % 10;
-            d = Math.floor(d / 10);
+            while (d > 0) {            
+                last = d % 10;
+                d = Math.floor(d / 10);
+    
+                if (last === (d%10)) {
+                    if (++n > dubs)
+                        dubs = n;
+                } else {break;}
+            }
+            
+            //console.log(`${d}:${n}:${dubs}`);
+            n = 0;
+        }
 
-            if (last === (d%10)) {
-                repeat++;
-            } else {break;}
+        //console.log(d_msg);
+
+        if (d_msg != "") {
+            await interaction.reply(d_msg);
+        } else {
+            await interaction.reply('Huh?');
         }
 
         // Process dubs
-        switch (repeat) {
+        switch (dubs) {
             case 0:
                 break;
             case 1:
